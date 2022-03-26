5df2e91426ec3e5fc503b2fd32d6224181f06959
Abdulnaser Sheikh <67299192+abdulnsheikh@users.noreply.github.com>
2022-03-25 22:55:47 -0500

    simple-junit-examples
    simple-junit-examples

---
+++ simple-junit-examples/src/q2/csvfilesource/TestComputation_CsvFileSource.java
@@ -0,0 +1,28 @@
+package q2.csvfilesource;
+
+import org.junit.Assert;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+
+
+public class TestComputation_CsvFileSource {
+
+   // TODO: Write your answer to complete this test case
+   @ParameterizedTest //(name = "{index} => input1={0}, input2= {1}, result={2}")
+   @CsvFileSource(resources = "computation_dataset.csv")
+  
+   public void testGetDiscount(int input1, 
+		   int input2, 
+		   int expected) throws Exception {
+
+	       Computation com = new Computation();
+	       
+	       
+	  int result = com.getDiscount(input1, input2);
+	  
+	  
+   Assert.assertEquals(result, expected);
+
+   }
+}
